b5b5e711da352b81e26e715f00d0692d6f42cdf5
Ashim Saha <flashim@gmail.com>
2019-03-18 22:08:50 +0530

    added

---
+++ precache-manifest.19aa62edeba7babc3bfc9aca6213534c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "0703af9ef3a5d06778b8",
+    "url": "./static/js/main.0703af9e.chunk.js"
+  },
+  {
+    "revision": "7c18b3cbf52fbd1c2cb9",
+    "url": "./static/js/1.7c18b3cb.chunk.js"
+  },
+  {
+    "revision": "0703af9ef3a5d06778b8",
+    "url": "./static/css/main.46ea16f7.chunk.css"
+  },
+  {
+    "revision": "f36211868194530cdcfa308f000e9853",
+    "url": "./index.html"
+  }
+];
